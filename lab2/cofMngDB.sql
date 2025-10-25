24f62a85836b1b03d32797e5db21cb0a7e4b5e33
MihajloVat <vatolkinmihajlo@gmail.com>
2025-10-25 18:14:22 +0300

    midwork save

--- lab2/cofMngDB.sql
+++ lab2/cofMngDB.sql
@@ -1,61 +1,64 @@
 CREATE TABLE IF NOT EXISTS Location (
-loc_id SERIAL PRIMARY KEY
+loc_id SERIAL PRIMARY KEY,
 address TEXT NOT NULL, 
 is_open BOOL NOT NULL,
-staff_count SMALLINT NOT NULL --derived 
 );
 
 CREATE TABLE IF NOT EXISTS Document(
-doc_id SERIAL 
-loc_id INT NOT NULL REFERENCES Location(loc_id) ON DELETE CASCADE, 
+doc_id SERIAL,
+loc_id INT NOT NULL REFERENCES Location(loc_id) ON DELETE CASCADE,--NOT NULL?
 title TEXT NOT NULL, 
 issue_date DATE,
+active_from DATE,
 expire_date DATE,
-is_active_m BOOLEAN, --think of m|d
-PRIMARY KEY(doc_id,loc_id)
+is_denied BOOLEAN NOT NULL DEFAULT false,
+PRIMARY KEY(doc_id)
 );
 
 CREATE TYPE pos_name AS ENUM ('бариста','прибиральник','адміністратор','кухар');
 
 CREATE TABLE IF NOT EXISTS Employee(
-emp_id SERIAL PRIMARY KEY
-full_name TEXT  NOT NULL, --think of structure
-age SMALLINT NOT NULL CHECK (age >= 16), 
-shift TEXT NOT NULL, --think of TYPE
+emp_id SERIAL PRIMARY KEY,
+loc_id INT NOT NULL REFERENCES Location(loc_id) ON DELETE CASCADE,--NOT NULL?
+surname VARCHAR(48)  NOT NULL, 
+name_ VARCHAR(32)  NOT NULL, 
+birthday DATE NOT NULL, 
+shift TEXT NOT NULL, 
 position_ pos_name,
 salary INT NOT NULL CHECK (salary > 0), 
-phone_number VARCHAR(13) NOT NULL CHECK (phone_number ~ '^\+380\d{9}$')
+phone_number CHAR(13) NOT NULL CHECK (phone_number ~ '^\+380\d{9}$')
 );
 
 CREATE TABLE IF NOT EXISTS EmployeeDoc(
-doc_id TEXT NOT NULL, 
-loc_id TEXT NOT NULL,
-emp_id TEXT NOT NULL, 
+doc_id INT NOT NULL, 
+emp_id INT NOT NULL, 
 
-FOREIGN KEY (doc_id ,loc_id)
- REFERENCES Document(doc_id,loc_id) ON DELETE CASCADE,
+FOREIGN KEY (doc_id)
+ REFERENCES Document(doc_id) ON DELETE CASCADE,
  
 FOREIGN KEY (emp_id)
  REFERENCES Employee(emp_id) ON DELETE CASCADE,
  
-PRIMARY KEY(doc_id ,loc_id,emp_id) 
+PRIMARY KEY(doc_id ,emp_id) 
 );
 
 CREATE TABLE IF NOT EXISTS Menu(
-menu_id SERIAL PRIMARY KEY
-title TEXT NOT NULL --think of TEXT
+menu_id SERIAL PRIMARY KEY,
+title VARCHAR(32) NOT NULL 
 ); 
 
 CREATE TABLE IF NOT EXISTS Item(
-item_id SERIAL PRIMARY KEY
-title TEXT NOT NULL, --think of TEXT
+item_id SERIAL PRIMARY KEY,
+menu_id INT NOT NULL REFERENCES Menu(menu_id) ON DELETE CASCADE --NOT NULL?
+title VARCHAR(32) NOT NULL, 
 current_price DECIMAL NOT NULL,
 recipe TEXT 
 );
 
 CREATE TABLE IF NOT EXISTS SoldOnDay(
-sold_id SERIAL
-item_title TEXT NOT NULL, --think of type?
+sold_id SERIAL,
+item_title VARCHAR(32), 
+item_id INT REFERENCES Item(item_id) ON DELETE SET NULL --NOT NULL?
 date DATE NOT NULL,
 price DECIMAL NOT NULL,
 amount SMALLINT NOT NULL CHECK (amount >= 0), 
@@ -63,70 +66,10 @@ PRIMARY KEY(sold_id,item_title)
 );
 
 --НЕ ЗАБУДЬ ФОРЕЙНИ!
---ПЕРЕПИСАТИ НА АЙДІШКИ
---Окремо ім'я і призвище?
---зміни шифт?
---think on NOT NULL on SERIAL
 --ПОДУМАЙ ПРО ЮНІК, ЧЕКИ І ДЕФОЛТИ
+--Перевірити зв'язки
 
 --EXEMPLES OF CONTENT
 
---location
-INSERT INTO Location 
-VALUES
-('Вул. Іво Бобула 14',true,3),
-('Пр. Кевіна Холланда 5\б',true,3),
-('Пл. Культури 10',false,2)
-ON CONFLICT (address) DO NOTHING;
-
---document
-INSERT INTO Document 
-VALUES
-('Договір про оренду', 'Вул. Іво Бобула 14', '2025-10-24', '2026-11-01', true),
-('Договір про оренду', 'Пр. Кевіна Холланда 5\б', '2025-10-24', '2026-11-01', true),
-('Дозвіл на торгівлю', 'Пр. Кевіна Холланда 5\б', '2025-10-24', NULL, true),
-('Договір на працевлаштування', 'Пр. Кевіна Холланда 5\б', '2025-10-24', NULL, true),
-('Документ про зміну посади','Пр. Кевіна Холланда 5\б', '2025-11-24', NULL, true)
-ON CONFLICT (title,loc_address) DO NOTHING;
-
---employee
-INSERT INTO Employee
-VALUES
-('Ватолкін Михайло',18,'10:00 - 20:00','бариста',17000,'+380966555734'),
-('Михайлов Антон',23,'18:00 - 20:00','прибиральник',14000,'+380956757733'),
-('Антоненко Андрій',25,'10:00 - 20:00','адміністратор',20000,'+380931111711')
-ON CONFLICT (full_name) DO NOTHING;
-
---employeeDoc
-INSERT INTO EmployeeDoc 
-VALUES
-('Договір на працевлаштування','Пр. Кевіна Холланда 5\б', 'Ватолкін Михайло'),
-('Договір на працевлаштування','Пр. Кевіна Холланда 5\б', 'Михайлов Антон'),
-('Договір на працевлаштування','Пр. Кевіна Холланда 5\б', 'Антоненко Андрій'),
-('Документ про зміну посади','Пр. Кевіна Холланда 5\б', 'Антоненко Андрій')
-ON CONFLICT (doc_title,loc_address,full_name) DO NOTHING;
-
---menu
-INSERT INTO Menu
-VALUES
-('Холодні напої'),
-('Кава'),
-('Солодощі')
-ON CONFLICT (title) DO NOTHING;
-
---item
-INSERT INTO Item 
-VALUES
-('Капучино',50,'*рецепт капучино*'),
-('Піна-колада',70,'*рецепт піни-колади*'),
-('Тістечко шоколадне', 50, NULL)
-ON CONFLICT (title) DO NOTHING;
-
-INSERT INTO SoldOnDay
-VALUES
-('2025-10-24','Капучино', 50, 104),
-('2025-10-24','Піна-колада', 70, 43),
-('2025-10-24','Тістечко шоколадне',50, 20)
-ON CONFLICT (date,item_title) DO NOTHING 
 
 
