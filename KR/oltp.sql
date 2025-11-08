3d082bb6dc57b7aa9fe814ab40d78f0e6fb10466
MihajloVat <vatolkinmihajlo@gmail.com>
2025-11-08 17:07:55 +0200

    OLTP now done 100%

--- KR/oltp.sql
+++ KR/oltp.sql
@@ -1,17 +1,23 @@
-INSERT INTO Projects(title,descr,s_date,dline) VALUES
-('Контрольна з баз даних', NULL,'2025-11-08','2025-11-08 23:59:59'),
-('Курсова з баз даних', 'Застосунок для менеджменту мережі кав''ярень','2025-11-08','2025-11-08 23:59:59')
+--INSERT INTO Projects(title,descr,s_date,dline) VALUES
+--('Контрольна з баз даних', NULL,'2025-11-08','2025-11-08 23:59:59'),
+--('Курсова з баз даних', 'Застосунок для менеджменту мережі кав''ярень','2025-11-08','2025-11-08 23:59:59')
 
-UPDATE Projects
-SET s_date = DEFAULT
-WHERE title = 'Контрольна з баз даних';
+--UPDATE Projects
+--SET s_date = DEFAULT
+--WHERE title = 'Контрольна з баз даних';
 
-UPDATE Projects
-SET descr = 'Передумав, придумаю іншу тему'
-WHERE title = 'Курсова з баз даних';\
+--UPDATE Projects
+--SET descr = 'Передумав, придумаю іншу тему'
+--WHERE title = 'Курсова з баз даних';
 
-Delete from Projects
-WHERE title = 'Контрольна з баз даних';
+--Delete from Projects
+--WHERE title = 'Контрольна з баз даних';
 
-Delete from Projects
-WHERE descr = 'Застосунок для менеджменту мережі кав''ярень'; --не спрацює, був апдейт
+--Delete from Projects
+--WHERE descr = 'Застосунок для менеджменту мережі кав''ярень'; --не спрацює, був апдейт
+
+SELECT task_id FROM Assignment
+WHERE user_id = 1;
+
+SELECT * FROM Tasks
+WHERE prio = 3
